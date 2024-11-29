a7541a8ad9d14ccb4d292c595d9f180694d324b3
11-10-01-Vasanth <mvasanth11102001@gmail.com>
2024-11-29 19:57:26 +0530

    Demo1

---
+++ Demo1.java
@@ -0,0 +1,21 @@
+class A {
+    {
+        System.out.println("Instance block");
+    }
+
+    static {
+        System.out.println("Static block");
+    }
+
+    public static void show() {
+        // TODO Auto-generated method stub
+        System.out.println("Static show block");
+    }
+}
+
+public class Demo1 {
+    public static void main(String[] args) {
+        // A a = new A();
+        A.show();
+    }
+}
